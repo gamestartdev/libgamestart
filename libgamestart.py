4f701f621f47974f22b346698047f49fa87a746e
nate <you@example.com>
2015-02-13 00:52:16 -0500

    renaming mc object for compatibility with other people's code

--- libgamestart.py
+++ libgamestart.py
@@ -13,48 +13,33 @@ __author__ = 'gamestart'
 MINECRAFT_SERVER = 'localhost'
 TNT = Block(TNT.id, 1)
 
-def get_ip_postfix():
-    return int(socket.gethostname().lstrip('pi'))
-
-
-def is_hosting_minecraft():
-    return bool(get_ip_postfix() % 2)  #Odd numbers host Minecraft for the previous Pi
-
-
 def get_server_name():
-    return MINECRAFT_SERVER or "192.168.3.%s" % ((((int(get_ip_postfix()) - 1) / 4) ) * 4 + 1)
+    return MINECRAFT_SERVER
 
-
-local_mincraft = minecraft.Minecraft.create()
-server_minecraft = minecraft.Minecraft.create(get_server_name())
-
+mc = minecraft.Minecraft.create()
+mc_server = minecraft.Minecraft.create(get_server_name())
 
 def setblock_at_pos(pos, blocktype=DIRT):
     setblock(pos.x, pos.y, pos.z, blocktype)
 
-
 def setblock(*args):
-    server_minecraft.setBlock(*args)
-
+    mc_server.setBlock(*args)
 
 def cube(px, py, pz, blocktype, size):
     size = min(size, 12)
     for x in range(size):
         for y in range(size):
             for z in range(size):
-                server_minecraft.setBlock(x + px, y + py, z + pz, blocktype)
-
+                mc_server.setBlock(x + px, y + py, z + pz, blocktype)
 
 def teleport_at_pos(pos):
     teleport(pos.x, pos.y, pos.z)
 
-
 def teleport(x, y, z):
-    local_mincraft.player.setPos(x, y, z)
-
+    mc.player.setPos(x, y, z)
 
 def my_pos():
-    return local_mincraft.player.getPos()
+    return mc.player.getPos()
 
 class Player():
     def __init__(self, mc_player):
@@ -76,17 +61,17 @@ class Player():
     def z(self):
         return self.mc_player.getPos().z
 
-player = Player(local_mincraft.player)
+player = Player(mc.player)
 
 def pick_random_player():
-    listOfPlayerEntityIds = server_minecraft.getPlayerEntityIds()
+    listOfPlayerEntityIds = mc_server.getPlayerEntityIds()
     print("Players: " + str(len(listOfPlayerEntityIds)))
     chosenPlayerId = random.choice(listOfPlayerEntityIds)
     print("Player to report position" + str(chosenPlayerId))
     return chosenPlayerId
 
-
 def yell(stringToYell):
-    local_mincraft.postToChat(stringToYell)
+    mc.postToChat(stringToYell)
 
 print "Starting..."
+
